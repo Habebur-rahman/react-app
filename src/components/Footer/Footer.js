ffd4f627b115a4eb6f9735188c68b59d5ca11b86
Habibur_Rahman <hr16792@gmail.com>
2024-01-08 20:48:27 +0600

    footer section added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <h4>Copyright © 2024 Callum. All Rights Reserved. </h4>
+            <h4> Designed by : Habibur Rahman </h4>
+            
+        </div>
+    );
+};
+
+export default Footer;
